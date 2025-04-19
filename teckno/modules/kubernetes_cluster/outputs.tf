d1085d793af212a51f41a9621fab4785a93c1b9a
mahad ahmed <mahad.ahmed@otmail.com>
2025-04-19 20:32:03 +0100

    added monitoring log analytics workspace

--- teckno/modules/kubernetes_cluster/outputs.tf
+++ teckno/modules/kubernetes_cluster/outputs.tf
@@ -1,3 +1,7 @@
 output "aks_principal_id" {
   value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
 }
+output "aks_id" {
+  value = azurerm_kubernetes_cluster.aks.id
+  
+}
---
+++ teckno/modules/monitoring/variables.tf
@@ -0,0 +1,48 @@
+variable "resource_group_name" {
+  description = "The name of the resource group containing AKS and ACR"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resources are deployed"
+  type        = string
+}
+
+
+variable "log_analytics_workspace_name" {
+  description = "The ID of the Log Analytics workspace for diagnostics"
+  type        = string
+  default = "teckno-law"
+}
+
+
+variable "aks_cluster_id" {
+  description = "The resource ID of the AKS cluster"
+  type        = string
+}
+
+# Log Analytics SKU
+variable "sku" {
+  description = "Log analytics workspace SKU"
+  type        = string
+  default     = "PerGB2018"
+}
+
+
+variable "acr_id" {
+  description = "The resource ID of the Azure Container Registry"
+  type        = string
+}
+
+# Optional naming variables for diagnostic settings
+variable "diagnostic_setting_name_aks" {
+  description = "Name for the AKS diagnostic setting"
+  type        = string
+  default     = "aks-diagnostics"
+}
+
+variable "diagnostic_setting_name_acr" {
+  description = "Name for the ACR diagnostic setting"
+  type        = string
+  default     = "acr-diagnostics"
+}
