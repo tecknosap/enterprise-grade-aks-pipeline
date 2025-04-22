0daf353ca524d53dd722b38f4578caae83fa4b9d
mahad ahmed <mahad.ahmed@otmail.com>
2025-04-22 21:48:57 +0100

    updated the monitoring module varaibles

--- teckno/modules/monitoring/main.tf
+++ teckno/modules/monitoring/main.tf
@@ -1,32 +1,18 @@
 resource "azurerm_log_analytics_workspace" "teckno_law" {
-  name =var.log_analytics_workspace_name
-  resource_group_name  = var.resource_group_name
-  location = var.location
+  name                = var.log_analytics_workspace_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
   retention_in_days   = 30
-  sku                 = var.sku  
-  
+  sku                 = var.sku
 }
 
 resource "azurerm_monitor_diagnostic_setting" "acr-diagnostics" {
-  name = var.diagnostic_setting_name_acr
-  target_resource_id = var.acr_id
-   log_analytics_workspace_id = azurerm_log_analytics_workspace.teckno_law.id
+  name                     = var.diagnostic_setting_name_acr
+  target_resource_id       = var.acr_id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.teckno_law.id
 
-     metric {
-    category = "AllMetrics"
-    enabled  = true
+  enabled_log {
+    category = "ContainerRegistryRepositoryEvents" # Updated to use a supported log category
   }
-
 }
 
-resource "azurerm_monitor_diagnostic_setting" "aks-diagnostics" {
-  name = var.diagnostic_setting_name_aks
-  target_resource_id = var.aks_cluster_id
-   log_analytics_workspace_id = azurerm_log_analytics_workspace.teckno_law.id
-
-     metric {
-    category = "AllMetrics"
-    enabled  = true
-  }
-
-}
